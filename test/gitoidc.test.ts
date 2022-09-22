c4f4688d501b720c382a5f1b672b2af14a3c9f6c
Srinath Bala <srinathbala00@gmail.com>
2022-09-22 17:49:06 +0530

    Initial commit

---
+++ test/gitoidc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Gitoidc from '../lib/gitoidc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/gitoidc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Gitoidc.GitoidcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
